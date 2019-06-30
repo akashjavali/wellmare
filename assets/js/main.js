14407714a0774f1c86d0ebbc0b5ff7852fcb1689
Akash Javali <akashjavali@gmail.com>
2019-07-01 02:39:08 +0530

    almost done

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/* Swiper starts */
+
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+/* Swiper ends */
+
+/* Swiper gallery starts */
+
+var swiper = new Swiper('.swiper-gallery', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+/* Swiper gallery ends */
