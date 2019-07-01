625c1d3b94dedb4e2dad20ba4fe875b9d1eb853b
Akash Javali <akashjavali@gmail.com>
2019-07-01 23:13:11 +0530

    added autoplay

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,10 @@ var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     centeredSlides: true,
     loop: true,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
