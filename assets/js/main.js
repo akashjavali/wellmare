21e44e549e551982678a7db1665f6bf441d8d5d9
Akash Javali <akashjavali@gmail.com>
2019-07-01 23:12:03 +0530

    added final touch

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,11 +35,76 @@ var swiper = new Swiper('.swiper-gallery', {
         el: '.swiper-pagination',
         clickable: true,
     },
+    breakpoints: {
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        575: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
 });
 
 
 /* Swiper gallery ends */
+
+
+
+/* navbar onscroll color change functionality */
+$(document).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $(".fixed-top").addClass("change_navColor", "main_nav");
+    } else {
+        $(".fixed-top").removeClass("change_navColor", "main_nav");
+    }
+});
+
+
+
+/* scroll to anchor */
+$(".nav_home").click(function () {
+    $("html, body").animate({
+            scrollTop: $(".hero").offset().top
+        },
+        800
+    );
+});
+$(".nav_about").click(function () {
+    $("html, body").animate({
+            scrollTop: $(".about").offset().top
+        },
+        800
+    );
+});
+$(".nav_products").click(function () {
+    $("html, body").animate({
+            scrollTop: $(".products").offset().top
+        },
+        800
+    );
+});
+$(".nav_gallery").click(function () {
+    $("html, body").animate({
+            scrollTop: $(".gallery").offset().top
+        },
+        800
+    );
+});
+$(".nav_contact").click(function () {
+    $("html, body").animate({
+            scrollTop: $(".contactus").offset().top
+        },
+        800
+    );
+});
